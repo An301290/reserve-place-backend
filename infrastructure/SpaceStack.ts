76a3bb7e1c29eefdb0f297577fc3a41854003de6
angel.samano <angel.samano@softwareone.com>
2023-01-02 16:38:33 +0100

    configure-Lambda-nodejs

--- infrastructure/SpaceStack.ts
+++ infrastructure/SpaceStack.ts
@@ -8,6 +8,7 @@ import {
 import { join } from "path";
 import { LambdaIntegration, RestApi } from "aws-cdk-lib/aws-apigateway";
 import { GenericTable } from "./GenericTable";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 
 export class SpaceStack extends Stack {
   //to initialize RestApi
@@ -23,6 +24,11 @@ export class SpaceStack extends Stack {
       runtime: Runtime.NODEJS_14_X,
       code: Code.fromAsset(join(__dirname, "..", "services", "hello")),
       handler: "hello.main",
+    });
+
+    const helloLambdaNodeJs = new NodejsFunction(this, "helloLambdaNodeJs", {
+      entry: join(__dirname, "..", "services", "node-lambda", "hello.ts"),
+      handler: "handler",
     });
 
     //Api Lambda Integration
